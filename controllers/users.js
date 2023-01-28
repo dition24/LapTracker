a6c4f711761d1018b20d4c6ea7eb16d1646fadc0
Daniel Placencia <Dition24@Gmail.com>
2023-01-28 16:06:46 -0500

    wip: sets sign up page and route

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
+router.get("/signup", (req, res) => {
+    res.render("signup.ejs", {error: null});
+});
+
 router.get("/login", (req, res) => {
     res.render("login.ejs", {error: null});
 });
